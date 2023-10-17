542c20434de2aefd9b9bff7e0b94f7ce5fefbad1
yurza <yurzaachmad@gmail.com>
2023-10-17 10:12:37 +0700

    update  delete

--- server/resources/js/components/Study/StudyIndex.vue
+++ server/resources/js/components/Study/StudyIndex.vue
@@ -46,7 +46,20 @@
                     >
                         {{ item.isEdit ? "Save" : "Edit" }}
                     </button>
+                    &nbsp;
+                    <button
+                        type="button"
+                        class="btn btn-danger"
+                        @click="showConfirmDialog(item)"
+                    >
+                        Delete
+                    </button>
                 </td>
+                <div v-if="item.isDelete" class="confirm-dialog">
+                    <p>Are you sure you want to remove this?</p>
+                    <button @click="removeContract(item.id)">Yes</button>
+                    <button @click="showConfirmDialog(item)">No</button>
+                </div>
             </tr>
         </tbody>
     </table>
@@ -62,6 +75,20 @@ export default {
         const toggleEdit = (item) => {
             item.isEdit = !item.isEdit;
         };
+        const showConfirmDialog = (item) => {
+            item.isDelete = !item.isDelete;
+        };
+
+        async function removeContract(id) {
+            await axios
+                .delete(`/api/studies/${id}`)
+                .then((response) => {
+                    location.reload();
+                })
+                .catch((error) => {
+                    console.log("Error", error);
+                });
+        }
 
         onMounted(() => {
             axios
@@ -80,7 +107,41 @@ export default {
         return {
             studies,
             toggleEdit,
+            showConfirmDialog,
+            removeContract,
         };
     },
 };
 </script>
+<style scoped>
+.confirm-dialog {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    border: 1px solid #ccc;
+    padding: 20px;
+    text-align: center;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+    z-index: 999;
+}
+
+.confirm-dialog p {
+    margin-bottom: 20px;
+}
+
+.confirm-dialog button {
+    margin: 0 10px;
+    padding: 10px 20px;
+    background-color: #ff0000;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.confirm-dialog button:hover {
+    background-color: #ffee00;
+}
+</style>
